4ed366a5bb7641b91680276d04479fcd0491acc8
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-11-23 21:54:30 -0300

    fix: update container form

--- src/app/auth/_components/auth.form.tsx
+++ src/app/auth/_components/auth.form.tsx
@@ -3,15 +3,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Link } from "lucide-react";
 
 export function AuthForm() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-black via-gray-900 to-red-800">
       <div className="p-6 bg-black rounded-lg shadow-lg border-2 border-red-600 max-w-md w-full">
         <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold text-red-600">Akatsuki Login</h1>
+          <h1 className="text-3xl font-bold text-red-600">InfineteSaas Login</h1>
           <p className="text-sm text-gray-400">
-            Entre para fazer parte da organização!
+            Entre para fazer parte Equipe!
           </p>
         </div>
         <form className="space-y-6 mt-4">
@@ -46,9 +47,9 @@ export function AuthForm() {
         </form>
         <div className="mt-4 text-center text-gray-400 text-sm">
           Não tem uma conta?{" "}
-          <a href="#" className="text-red-500 underline">
+          <Link href="#" className="text-red-500 underline">
             Cadastre-se
-          </a>
+          </Link>
         </div>
       </div>
     </div>
