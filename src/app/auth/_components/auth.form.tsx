47baad5e8df8c23a8b8c14e0c3a9189be6921f33
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-11-25 19:54:17 -0300

    feat: adicionando heact hook form

--- src/app/auth/_components/auth.form.tsx
+++ src/app/auth/_components/auth.form.tsx
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Link } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Link } from "lucide-react"
 
 export function AuthForm() {
   return (
@@ -53,5 +53,5 @@ export function AuthForm() {
         </div>
       </div>
     </div>
-  );
+  )
 }
